dbc278304feedbb4c8577e09e5e3c6dba844db1a
Gustavo F. de Farias <gff@cin.ufpe.br>
2019-12-30 15:31:24 -0300

    Added the template parameter to Range Sum Binary Indexed Tree

--- data-structures/binary-indexed-tree/range-sum-bit.cpp
+++ data-structures/binary-indexed-tree/range-sum-bit.cpp
@@ -3,8 +3,9 @@ using namespace std;
 
 typedef long long ll;
 
+template <typename T>
 struct RangeSum {
-    vector<ll> bit;
+    vector<T> bit;
     int n;
     RangeSum (int n) :
         bit(n + 1, 0),
@@ -13,19 +14,19 @@ struct RangeSum {
     int lso (int i) {
         return (i & (-i));
     }
-    void update (int i, ll delta) {
+    void update (int i, T delta) {
         for (int j = i; j <= n; j += lso(j)) {
             bit[j] += delta;
         }
     }
-    ll query (int i) {
-        ll sum = 0;
+    T query (int i) {
+        T sum = 0;
         for (int j = i; j > 0; j -= lso(j)) {
             sum += bit[j];
         }
         return sum;
     }
-    ll range (int l, int r) {
+    T range (int l, int r) {
         return query(r) - query(l - 1);
     }
 };
