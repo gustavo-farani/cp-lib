6cd013e3df833f196f1391a08b8fe4593c1dc86a
Gustavo F. de Farias <gff@cin.ufpe.br>
2020-01-05 10:56:11 -0300

    Added Tarjan's Algorithm for finding Strongly Connected Componets (included method for building condensation graph)

---
+++ graph/traversal/strongly-connected-componets/tarjan.cpp
@@ -0,0 +1,70 @@
+#include <vector>
+#include <stack>
+#include <algorithm>
+#include "../topological-sort/kahn.cpp"   // DAG class
+using namespace std;
+
+#define pb push_back
+typedef vector<int> vi;
+typedef pair<int, int> ii;
+
+struct Digraph {                // directed graph
+    int n, cnt;
+    vector<vi> adj;
+    vi pre, low;
+    vector<vi> p;               // partition of the vertices
+    stack<int> s;
+    vector<bool> on;
+    Digraph (int n) :           // 1-based
+        n(n), cnt(0), adj(n + 1),
+        pre(n + 1), low(n + 1), on(n + 1)
+    {}
+    void addEdge (int u, int v) {
+        adj[u].pb(v);
+    }
+    int scc () {                // build and counts strongly connected components
+        for (int i = 1; i <= n; i++) {
+            if (pre[i] == 0) dfs(i, i);
+        }
+        return p.size();
+    }
+    DAG condensationGraph () {
+        int k = scc();
+        vi rep(n + 1);
+        for (int i = 1; i <= k; i++) {
+            for (int u : p[i - 1]) rep[u] = i;
+        }
+        vector<ii> edges;
+        for (int u = 1; u <= n; u++) {
+            for (int v : adj[u]) {
+                if (rep[u] != rep[v]) edges.pb({rep[u], rep[v]});
+            }
+        }
+        int m = unique(edges.begin(), edges.end()) - edges.begin();
+        DAG c(k);
+        for (int i = 0; i < m; i++) {
+            c.addEdge(edges[i].first, edges[i].second);
+        }
+        return c;
+    }
+    void dfs (int u, int par) {
+        low[u] = pre[u] = ++cnt;
+        s.push(u);
+        on[u] = true;
+        for (int v : adj[u]) {
+            if (pre[v] == 0) dfs(v, u);
+            if (on[v]) low[u] = min(low[u], low[v]);
+        }
+        if (low[u] == pre[u]) {
+            p.pb(vi());
+            bool end = false;
+            while (!end) {
+                int v = s.top();
+                p.back().pb(v);
+                s.pop();
+                on[v] = false;
+                end = (u == v);
+            }
+        }
+    }
+};
