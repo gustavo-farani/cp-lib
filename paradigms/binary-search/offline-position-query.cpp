59ee4ef4dc99344b4bf1da67f065b4e885c092fd
Gustavo F. de Farias <gff@cin.ufpe.br>
2020-01-07 14:17:12 -0300

    Optimized Offline Position Query class

--- paradigms/binary-search/offline-position-query.cpp
+++ paradigms/binary-search/offline-position-query.cpp
@@ -7,16 +7,14 @@ using namespace std;
 template<typename T>
 struct Universe {   // Offline Position Query
     vector<T> o;
-    int sz;
-    void push (T x) { o.pb(x); }
-    int build () {   // returns number of distinct elements
+    void push (const T &x) { o.pb(x); }
+    void build () {
         sort(o.begin(), o.end());
-        return sz = unique(o.begin(), o.end()) - o.begin();
+        o.resize(unique(o.begin(), o.end()) - o.begin());
     }
-    int rank (T x) {   // 1-based
-        auto it = lower_bound(o.begin(), o.begin() + sz, x);
-        int i = (it - o.begin());
-        return (i == sz || x < o[i] ? -1 : i + 1);
+    int find (const T &x) {   // 0-based
+        auto it = lower_bound(o.begin(), o.end(), x);
+        return (it == o.end() || x < *it ? -1 : it - o.begin());
     }
-    T operator[] (int i) { return o[i - 1]; }  // 1-based
+    T& operator[] (int i) { return o[i]; }  // 0-based
 };
