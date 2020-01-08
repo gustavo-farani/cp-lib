855f5598dafc2d9150b2e8a38a76cf22c5dd151c
Gustavo F. de Farias <gff@cin.ufpe.br>
2020-01-08 16:33:56 -0300

    Added some extra methods for Union-Find data-structure

--- data-structures/disjoint-sets-union.cpp
+++ data-structures/disjoint-sets-union.cpp
@@ -5,8 +5,9 @@ using namespace std;
 typedef vector<int> vi;
 
 struct DSU {
+    int n;
     vi par, sz;
-    DSU (int n) : par(1), sz(n + 1, 1) {
+    DSU (int n) : n(n), par(1), sz(n + 1, 1) { // 1-based
         for (int i = 1; i <= n; i++) par.pb(i);
     }
     int find (int u) {
@@ -20,10 +21,15 @@ struct DSU {
         u = find(u);
         v = find(v);
         if (u != v) {
+            n--;
             if (sz[u] < sz[v]) swap(u, v);
             par[v] = u;
             sz[u] += sz[v];
         }
     }
+    bool tied (int u, int v) {
+        return (find(u) == find(v));
+    }
     int count (int u) { return sz[find(u)]; }
+    int size () { return n; }
 };
