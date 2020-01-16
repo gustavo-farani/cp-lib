ad527dffd53efd787dc9a1c699d3b4f8501875c3
Gustavo F. de Farias <gff@cin.ufpe.br>
2020-01-16 01:00:07 -0300

    Code for lazy propagation's range increment and assignment updates (with sum queries) made shorter and more efficient

--- data-structures/segment-tree/lazy-progation/range-assignment-update-range-sum-query.cpp
+++ data-structures/segment-tree/lazy-progation/range-assignment-update-range-sum-query.cpp
@@ -1,23 +1,50 @@
+#include <vector>
+#include <tuple>
+using namespace std;
+
 typedef long long ll;
 
-struct Node {
-    ll sum;
-    Node operator+ (const Node &o) {
-        return Node{sum + o.sum};
+struct RangeAssign {
+    vector<ll> tree;
+    vector< pair<bool, int> > lazy;
+    RangeAssign (int n) :
+        tree(n << 2), lazy(n << 2)
+    {}
+    void refresh (int i, int l, int r) {
+        if (lazy[i].first) {
+            tree[i] = (ll) (r - l + 1) * lazy[i].second;
+            if (l < r) {
+                int lc = i << 1, rc = (i << 1) + 1;
+                lazy[lc] = lazy[rc] = lazy[i];
+            }
+            lazy[i].first = false;
+        }
     }
-    void assign (int x, int l, int r) {
-        sum = (ll) x * (r - l + 1);
+    void update (int i, int l, int r, int ul, int ur, int x) {
+        refresh(i, l, r);
+        if (r < ul || l > ur) {
+            return;
+        } else if (ul <= l && r <= ur) {
+            lazy[i] = {true, x};
+            refresh(i, l, r);
+        } else {
+            int m = (l + r) >> 1;
+            int lc = i << 1, rc = (i << 1) + 1;
+            update(lc, l, m, ul, ur, x);
+            update(rc, m + 1, r, ul, ur, x);
+            tree[i] = tree[lc] + tree[rc];
+        }
     }
-} NIL{0};
-
-struct Todo {
-    bool status;
-    int val;
-    bool empty () { return !status; }
-    void pop () { status = false; }
-    void push (int x) {
-        status = true;
-        val = x;
+    ll query (int i, int l, int r, int ql, int qr) {
+        refresh(i, l, r);
+        if (r < ql || l > qr) {
+            return 0;
+        } else if (ql <= l && r <= qr) {
+            return tree[i];
+        } else {
+            int m = (l + r) >> 1;
+            int lc = i << 1, rc = (i << 1) + 1;
+            return query(lc, l, m, ql, qr) + query(rc, m + 1, r, ql, qr);
+        }
     }
-    int top () { return val; }
 };
--- data-structures/segment-tree/lazy-progation/range-increment-update-range-sum-query.cpp
+++ data-structures/segment-tree/lazy-progation/range-increment-update-range-sum-query.cpp
@@ -1,19 +1,49 @@
+#include <vector>
+using namespace std;
+
 typedef long long ll;
 
-struct Node {
-    ll sum;
-    Node operator+ (const Node& o) {
-        return Node{sum + o.sum};
+struct RangeIncrement {
+    vector<ll> tree, lazy;
+    RangeIncrement (int n) :
+        tree(n << 2), lazy(n << 2)
+    {}
+    void refresh (int i, int l, int r) {
+        if (lazy[i] != 0) {
+            tree[i] += lazy[i] * (r - l + 1);
+            if (l < r) {
+                int lc = i << 1, rc = (i << 1) + 1;
+                lazy[lc] += lazy[i];
+                lazy[rc] += lazy[i];
+            }
+            lazy[i] = 0;
+        }
     }
-    void assign (ll x, int l, int r) {
-        sum += x * (r - l + 1);
+    void update (int i, int l, int r, int ul, int ur, ll x) {
+        refresh(i, l, r);
+        if (r < ul || l > ur) {
+            return;
+        } else if (ul <= l && r <= ur) {
+            lazy[i] += x;
+            refresh(i, l, r);
+        } else {
+            int m = (l + r) >> 1;
+            int lc = i << 1, rc = (i << 1) + 1;
+            update(lc, l, m, ul, ur, x);
+            update(rc, m + 1, r, ul, ur, x);
+            tree[i] = tree[lc] + tree[rc];
+        }
     }
-} NIL{0};
-
-struct Todo {
-    ll add;
-    ll top () { return add; }
-    void push (ll x) { add += x; }
-    bool empty () { return (add == 0); }
-    void pop () { add = 0; }
+    ll query (int i, int l, int r, int ql, int qr) {
+        refresh(i, l, r);
+        if (r < ql || l > qr) {
+            return 0;
+        } else if (ql <= l && r <= qr) {
+            return tree[i];
+        } else {
+            int m = (l + r) >> 1;
+            int lc = i << 1, rc = (i << 1) + 1;
+            return query(lc, l, m, ql, qr) + query(rc, m + 1, r, ql, qr);
+        }
+    }
 };
