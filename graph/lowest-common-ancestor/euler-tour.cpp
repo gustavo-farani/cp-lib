684a923ec74365a267f4774377a1c121ff36a3c2
Gustavo F. de Farias <gff@cin.ufpe.br>
2020-01-16 01:03:42 -0300

    Added LCA with Euler Tour, RMQ solved with Sparse Table

---
+++ graph/lowest-common-ancestor/euler-tour.cpp
@@ -0,0 +1,62 @@
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+#define pb push_back
+typedef vector<int> vi;
+
+struct LCA {
+    vector<vi> adj, st;
+    vi pre, lvl, euler, lg;
+    LCA (int n) :  // 0-based
+        adj(n), pre(n), lvl(n),
+        st((n << 1) - 1), lg(n << 1)
+    {}
+    void addEdge (int u, int v) {
+        adj[u].pb(v);
+        adj[v].pb(u);
+    }
+    void dfs (int u, int par) {
+        pre[u] = euler.size();
+        euler.pb(u);
+        for (int v : adj[u]) {
+            if (v != par) {
+                lvl[v] = lvl[u] + 1;
+                dfs(v, u);
+                euler.pb(u);
+            }
+        }
+    }
+    int merge (int u, int v) {
+        return (lvl[u] <= lvl[v] ? u : v);
+    }
+    void build () {
+        dfs(0, 0);
+        int m = euler.size();  // 2*(n - 1) + 1
+        for (int i = 2; i <= m; i++) {
+            lg[i] = lg[i >> 1] + 1;
+        }
+        for (int i = 0; i < m; i++) {
+            st[i].assign(lg[m - i] + 1, -1);
+            st[i][0] = euler[i];
+        }
+        for (int j = 1; j <= lg[m]; j++) {
+            for (int i = 0; i + (1 << j) <= m; i++) {
+                st[i][j] = merge(st[i][j - 1],
+                st[i + (1 << (j - 1))][j - 1]);
+            }
+        }
+    }
+    int query (int l, int r) {
+        int k = lg[r - l + 1];
+        return merge(st[l][k], st[r - (1 << k) + 1][k]);
+    }
+    int lca (int u, int v) {
+        if (pre[u] > pre[v]) swap(u, v);
+        return query(pre[u], pre[v]);
+    }
+    int dist (int u, int v) {
+        int w = lca(u, v);
+        return lvl[u] + lvl[v] - (lvl[w] << 1);
+    }
+};
