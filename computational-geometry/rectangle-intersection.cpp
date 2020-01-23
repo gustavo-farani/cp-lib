bc84fc915504b10cd7a4257c88c2335beb4e6b29
Gustavo F. de Farias <gff@cin.ufpe.br>
2020-01-23 17:00:13 -0300

    Added rectangle intersection on computational geometry

---
+++ computational-geometry/rectangle-intersection.cpp
@@ -0,0 +1,21 @@
+#include <tuple>
+#include <algorithm>
+using namespace std;
+
+typedef pair<int, int> ii;
+
+ii inter (ii a, ii b) {
+    if (b.first < a.first) swap(a, b);
+    return {b.first, min(a.second, b.second)};
+}
+
+struct Rect {
+    ii x, y;
+    Rect operator^ (Rect o) {
+        return Rect{inter(x, o.x), inter(y, o.y)};
+    }
+    bool empty () {
+        return (x.first > x.second
+        || y.first > y.second);
+    }
+};
