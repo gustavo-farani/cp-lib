2414ea728b888051144caca1a623f36c4c2166f2
Gustavo F. de Farias <gff@cin.ufpe.br>
2020-02-07 02:08:11 -0300

    Added comments explaining the parameters of stars and bars (combinations with repetitions) formula

--- math/combinatorics/combinations-with-repetitions.cpp
+++ math/combinatorics/combinations-with-repetitions.cpp
@@ -1,6 +1,10 @@
 #include "binomial-coefficient.cpp"
 #include "../util/mod-base.cpp"
 
+// number of non-negative solutions for linear equation:
+// x1 + ... + xn = k
+// n := number of variables / number of classes
+// k := independent term / number of instances
 ModBase starsAndBars (int n, int k) {
     return combinations(n + k - 1, k);
 }
