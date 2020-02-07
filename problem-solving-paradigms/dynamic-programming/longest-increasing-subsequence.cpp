e0d13e994a23dad8807028ebbe9cebbf6b61dfb8
Gustavo F. de Farias <gff@cin.ufpe.br>
2020-02-07 15:49:02 -0300

    Extended usability and optimized memory consumption in LIS

--- problem-solving-paradigms/dynamic-programming/longest-increasing-subsequence.cpp
+++ problem-solving-paradigms/dynamic-programming/longest-increasing-subsequence.cpp
@@ -2,23 +2,24 @@
 #include <algorithm>
 using namespace std;
 
+typedef vector<int> vi;
+
 template <class T>
 struct LIS {
-    int n;
-    vector<T> pivot, v;
+    vector<T> pivot;
     vi pred, pos, color;
     template <class I, class C>
     LIS (I first, I last, C comp) :
-        v(first, last), n(last - first),
-        pred(n), color(n)
+        pred(last - first), color(last - first)
     {
-        for (int i = 0; i < n; i++) {
-            int j = lower_bound(pivot.begin(), pivot.end(), v[i], comp) - pivot.begin();
+        for (I it = first; it != last; it++) {
+            int i = it - first;
+            int j = lower_bound(pivot.begin(), pivot.end(), *it, comp) - pivot.begin();
             if (j == pivot.size()) {
-                pivot.pb(v[i]);
+                pivot.pb(*it);
                 pos.pb(i);
             } else {
-                pivot[j] = v[i];
+                pivot[j] = *it;
                 pos[j] = i;
             }
             color[i] = j;
@@ -30,12 +31,12 @@ struct LIS {
     */
     int size () { return pivot.size(); }
     /*
-    An example of longest increasing subsequence (according to comp)
+    Returns the INDICES of some longest increasing subsequence (according to comp)
     */
-    vector<T> sample () {
+    vi restore () {
         vector<T> lis;
         for (int i = pos.back(); i != -1; i = pred[i]) {
-            lis.pb(v[i]);
+            lis.pb(i);
         }
         reverse(lis.begin(), lis.end());
         return lis;
