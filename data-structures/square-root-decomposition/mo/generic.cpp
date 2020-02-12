42e40ff903b671ceed4c0fd87f04f8e9eec24cc0
Gustavo F. de Farias <gff@cin.ufpe.br>
2020-02-12 13:48:45 -0300

    Enhanced usability and legibility of Mo's algorithm generic template

--- data-structures/square-root-decomposition/mo/generic.cpp
+++ data-structures/square-root-decomposition/mo/generic.cpp
@@ -4,53 +4,55 @@
 using namespace std;
     
 #define pb push_back
+typedef vector<int> vi;
 
-const int bs = 320;     // sqrt(1e5)
-    
+const int BS = 320;      // bucket-size
+
 struct Query {
     int l, r;    // closed interval: [l, r], 1-based indexation
     int id;      // priority number (entry time) of a query
     bool operator< (const Query &o) {
-        int i = (l - 1)/bs, j = (o.l - 1)/bs;
+        int i = (l - 1)/BS, j = (o.l - 1)/BS;
         if (i != j) {
             return (i < j);
         } else if (i & 1) {    // zig-zag optmization
-            return (r > o.r);  // odd bucket: right pointers in deacresing order
+            return (r > o.r);  // odd-indexed bucket: right pointers in deacresing order
         } else {
-            return (r < o.r);  // even bucket: right pointers in increasing order
+            return (r < o.r);  // even-indexed bucket: right pointers in increasing order
         }
     }
 };
 
-struct Answer{};
-
-struct DS {
-    Answer state;    // the final answer after the range is adjusted
-    DS ();
-    void operator+= (int i);  // process the addition of array's ith position to the range
-    void operator-= (int i);  // process the removal of array's ith position from the range
-} ds;
+struct Answer{}; // contains all the necessary data to be computed about a range
+// since a single integer value is usually enough, the struct definition may be
+// simply replaced by a:    typedef int Answer;     or     typedef long long Answer;
 
 struct Mo {
-    int cnt;  // queries id's are defined automatically and safely
+    vi v;                    // underlying array
+    int cnt;                 // queries id's are counted automatically and setted safely
     vector<Query> qrs;
-    Mo () : cnt(0) {}
-    void push (int l, int r) {  // pushes a new query to the back of the queue
+    Answer state;            // the final answer after the range boundaries are adjusted
+    Mo (int n) : v(n + 1), cnt(0) {}
+    int& operator[] (int i) { return v[i]; }             // for reading input inside main
+    void push (int l, int r) {             // pushes a new query to the back of the queue
         qrs.pb(Query{l, r, cnt++});
     }
+    void add (int i);    // TODO process the addition of array's ith position to the range
+    void remove (int i); // TODO process the removal of array's ith position from the range
     queue<Answer> solve () {
         vector<Answer> ans(qrs.size());
         sort(qrs.begin(), qrs.end());
         int l = 1, r = 0;             // following 1-based indexation
         for (Query q : qrs) {
-            while (l > q.l) ds += --l;
-            while (r < q.r) ds += ++r;
-            while (l < q.l) ds -= l++;
-            while (r > q.r) ds -= r--;
-            ans[q.id] = ds.state;
+            while (l > q.l) add(--l);
+            while (r < q.r) add(++r);
+            while (l < q.l) remove(l++);
+            while (r > q.r) remove(r--);
+            ans[q.id] = state;
         }
-        queue<Answer> q; // a queue dismiss the use of counters and iterators
+        queue<Answer> q; // a queue, rather than a vector, allows for successive retrievals independent from
+        // interation: the retrievals may not necessarily happen all at the same time, in a single for loop
         for (Answer a : ans) q.push(a);
         return q;
     }
-} mo;
+};
