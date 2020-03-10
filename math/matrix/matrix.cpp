a1b425c4e928343806dd81876c848e2c8949efd2
Gustavo F. de Farias <gff@cin.ufpe.br>
2020-03-10 18:50:42 -0300

    Adjusted matrix multiplication class

--- math/matrix/matrix.cpp
+++ math/matrix/matrix.cpp
@@ -2,33 +2,29 @@
 using namespace std;
 
 typedef long long ll;
-typedef vector<ll> Vector;
+typedef vector<int> vi;
 
-const int O = 3;
-ll m;
+const int MOD = 1e9 + 7;
 
 struct Matrix {
-	vector< Vector > e;
-	Matrix (int k) : e(O, Vector(O, 0)) {
-		for (int i = 0; i < O; i++) for (int j = 0; j < O; j++)
-			e[i][j] = k*(i == j);
+	int o;     // order (number of rows and columns) of the square matrix
+	vector<vi> e;
+	Matrix (int o, int k) : o(o), e(o, vi(o)) {
+		for (int i = 0; i < o; i++) for (int j = 0; j < o; j++)
+			e[i][j] = k*(i == j);    // initialized as kI := identity matrix scaled by k
 	}
-	Matrix operator* (Matrix a) {
-		Matrix p (0);
-		for (int i = 0; i < O; i++) for (int j = 0; j < O; j++)
-			for (int k = 0; k < O; k++) 
-				p.e[i][j] = (p.e[i][j] + e[i][k] * a.e[k][j]) % m;
+	Matrix operator* (const Matrix& a) {   // matrix-matrix multiplication
+		Matrix p(o, 0);
+		for (int i = 0; i < o; i++) for (int j = 0; j < o; j++)
+			for (int k = 0; k < o; k++)
+				p.e[i][j] = (p.e[i][j] + (ll) e[i][k]*a.e[k][j]) % MOD;
 		return p;
 	}
-	void operator= (const Matrix &a) {
-		for (int i = 0; i < O; i++) for (int j = 0; j < O; j++)
-			e[i][j] = a.e[i][j];
-	}
-	Vector operator* (const Vector &v) {
-		Vector u (O, 0);
-		for (int i = 0; i < O; i++)
-			for (int k = u[i] = 0; k < O; k++)
-				u[i] = (u[i] + e[i][k] * v[k]) % m;
+	vi operator* (const vi &v) {           // vector-matrix multiplication
+		vi u(o);
+		for (int i = 0; i < o; i++)
+			for (int k = u[i] = 0; k < o; k++)
+				u[i] = (u[i] + (ll) e[i][k]*v[k]) % MOD;
 		return u;
 	}
 };
