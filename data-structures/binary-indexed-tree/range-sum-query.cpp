788778b42400ec76016eac7bc516b01bacc0ca80
Gustavo F. de Farias <gff@cin.ufpe.br>
2020-03-10 17:16:51 -0300

    Simplfied the code for Binary Indexed Tree

--- data-structures/binary-indexed-tree/range-sum-query.cpp
+++ data-structures/binary-indexed-tree/range-sum-query.cpp
@@ -1,32 +1,19 @@
 #include <vector>
 using namespace std;
 
-typedef long long ll;
-
-template <typename T>
-struct RangeSum {   // Range Sum Query Binary Indexed Tree
-    vector<T> bit;
+template<class T>
+struct RangeSum {   // Binary Indexed Tree for Online Range Sum Query
     int n;
-    RangeSum (int n) :
-        bit(n + 1, 0),
-        n(n)
-    {}
-    int lso (int i) {
-        return (i & (-i));
+    vector<T> bit;
+    RangeSum (int n) : bit(n + 1, 0), n(n) {}
+    int lso (int i) { return i & (-i); }
+    void update (int i, T delta) {   // 1-based
+        for (int j = i; j <= n; j += lso(j)) bit[j] += delta;
     }
-    void update (int i, T delta) {
-        for (int j = i; j <= n; j += lso(j)) {
-            bit[j] += delta;
-        }
-    }
-    T query (int i) {
+    T prefix (int i) {               // 1-based
         T sum = 0;
-        for (int j = i; j > 0; j -= lso(j)) {
-            sum += bit[j];
-        }
+        for (int j = i; j > 0; j -= lso(j)) sum += bit[j];
         return sum;
     }
-    T range (int l, int r) {
-        return query(r) - query(l - 1);
-    }
+    T query (int l, int r) { return prefix(r) - prefix(l); }
 };
