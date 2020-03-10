cb94b06f702aa9cec67bbe10a3c944bb470b9423
Gustavo F. de Farias <gff@cin.ufpe.br>
2020-03-10 17:20:17 -0300

    Small fixes

--- data-structures/prefix-sum/2D-offline-sum-query.cpp
+++ data-structures/prefix-sum/2D-offline-sum-query.cpp
@@ -5,9 +5,7 @@ template<class T>
 struct PrefixSum2D {
     vector< vector<T> > p;
     PrefixSum2D (int n, int m) : p(n + 1, vector<T>(m + 1)) {}
-    T& at (int i, int j) {    // 1-based indexation
-        return p[i][j];
-    }
+    T& at (int i, int j) { return p[i][j]; }      // 1-based indexation
     void build (int n, int m) {
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= m; j++) {
@@ -15,7 +13,7 @@ struct PrefixSum2D {
             }
         }
     }
-    T query (int i1, int j1, int i2, int j2) { // 1-based indexation, closed interval
-        return (p[i2][j2] + p[i1 - 1][j1 - 1]) - (p[i2][j1 - 1] + p[i1 - 1][j2]);
+    T query (int r1, int c1, int r2, int c2) {    // 1-based indexation, closed interval
+        return (p[r2][c2] + p[r1 - 1][c1 - 1]) - (p[r2][c1 - 1] + p[r1 - 1][c2]);
     }
 };
