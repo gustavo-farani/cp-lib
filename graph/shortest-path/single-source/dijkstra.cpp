6cb7d5649ef6dfcaf014e3b06ad9720412df1bf0
Gustavo F. de Farias <gff@cin.ufpe.br>
2020-03-15 13:18:12 -0300

    Dijkstra Algorithm now uses underlying graph object

--- graph/shortest-path/single-source/dijkstra.cpp
+++ graph/shortest-path/single-source/dijkstra.cpp
@@ -2,65 +2,58 @@
 #include <queue>
 #include <tuple>
 #include <algorithm>
+#include "../../default/graph.cpp"
 using namespace std;
 
 #define pb push_back
 typedef long long ll;
-typedef pair<ll, int> Edge;
 typedef vector<int> vi;
+template<class T> using min_heap = priority_queue<T, vector<T>, greater<T>>;
 
 const ll INF = 1e18;
 
-struct Graph {     // Dijkstra's Algorithm
-    int n;
-    vector< vector<Edge> > adj;  // weighted graph
-    Graph (int n) : n(n), adj(n) {}   // 0-based
-    void addEdge (int u, int v, int w) {
-        adj[u].pb({w, v});    // undirected graph
-        adj[v].pb({w, u});
-    }
-    vector<ll> minimumDistances (int src) {
-        vector<ll> dist(n, INF);
-        priority_queue< Edge, vector<Edge>, greater<Edge> > margin;
-        margin.emplace(dist[src] = 0, src);
-        while (!margin.empty()) {
-            int u;
-            ll l;
-            tie (l, u) = margin.top();
-            margin.pop();
-            if (l <= dist[u]) {
-                for (Edge e : adj[u]) {
-                    int v, w;
-                    tie (w, v) = e;
-                    if (l + w < dist[v]) {
-                        margin.emplace(dist[v] = l + w, v);
-                    }
+template<class T>
+vector<ll> Graph<T>::minimumDistances (int s) {
+    vector<ll> dist(n + 1, INF);     // 1-based indexation
+    min_heap<pair<ll, int>> pq;
+    pq.emplace(dist[s] = 0, s);
+    while (!pq.empty()) {
+        int u; ll l;
+        tie (l, u) = pq.top(); pq.pop();
+        // simply auto [l, u] = pq.top(); pq.pop(); for C++ 17
+        if (l <= dist[u]) {
+            for (int z : adj[u]) {
+                int v = u ^ edges[z].from ^ edges[z].to;
+                ll w = edges[z].weigth;
+                if (l + w < dist[v]) {
+                    pq.emplace(dist[v] = l + w, v);
                 }
             }
         }
-        return dist;
     }
-    vi shortestPaths (int src) {
-        vector<ll> dist(n, INF);
-        vi from(n);
-        priority_queue< Edge, vector<Edge>, greater<Edge> > margin;
-        margin.emplace(dist[src] = 0, src);
-        while (!margin.empty()) {
-            int u;
-            ll l;
-            tie (l, u) = margin.top();
-            margin.pop();
-            if (l <= dist[u]) {
-                for (Edge e : adj[u]) {
-                    int v, w;
-                    tie (w, v) = e;
-                    if (l + w < dist[v]) {
-                        from[v] = u;
-                        margin.emplace(dist[v] = l + w, v);
-                    }
+    return dist;
+}
+
+template<class T>
+vi Graph<T>::shortestPaths (int s) {
+    vector<ll> dist(n + 1, INF);    // 1-based
+    vi from(n + 1);                 // 1-based
+    min_heap<pair<ll, int>> pq;
+    pq.emplace(dist[s] = 0, s);
+    while (!pq.empty()) {
+        int u; ll l;
+        tie (l, u) = pq.top(); pq.pop();
+        /* simply auto [l, u] = pq.top(); pq.pop(); for C++ 17 */
+        if (l <= dist[u]) {
+            for (int z : adj[u]) {
+                int v = u ^ edges[z].from ^ edges[z].to;
+                ll w = edges[z].weigth;
+                if (l + w < dist[v]) {
+                    from[v] = u;
+                    pq.emplace(dist[v] = l + w, v);
                 }
             }
         }
-        return from;
     }
-};
+    return from;
+}
