9b01e630ebb039a8500b0b58a9365c9ec7d435a4
Gustavo F. de Farias <gff@cin.ufpe.br>
2020-03-21 02:23:43 -0300

    Added binary search templates

---
+++ problem-solving-paradigms/binary-search/monotonically-decreasing.cpp
@@ -0,0 +1,10 @@
+template<class F>
+int search (int l, int r, F test) { // 111[1]00
+    r++;
+    while (r - l > 1) {
+        int m = l + r >> 1;
+        if (test(m)) l = m;
+        else r = m;
+    }
+    return (test(l) ? l : l - 1);
+}
---
+++ problem-solving-paradigms/binary-search/monotonically-increasing.cpp
@@ -0,0 +1,9 @@
+template<class F>
+int search (int l, int r, F test) { // 000[1]11
+    while (l < r) {
+        int m = l + r >> 1;
+        if (test(m)) r = m;
+        else l = m + 1;
+    }
+    return (test(l) ? l : l + 1);
+}
