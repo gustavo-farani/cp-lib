41aab1e6a1ebac13df7cb795951d9c013be6d0ac
Gustavo F. de Farias <gff@cin.ufpe.br>
2020-03-21 00:38:46 -0300

    updated DSU disjoint-sets-count

--- data-structures/disjoint-sets-union/disjoint-sets-count.cpp
+++ data-structures/disjoint-sets-union/disjoint-sets-count.cpp
@@ -5,26 +5,20 @@ using namespace std;
 #define pb push_back
 typedef vector<int> vi;
 
-struct DSU {         // union by size
-    int n;           // number of separate trees (disjoint sets) in forest
+struct DSU {       // union by size
+    int n;
     vi par, sz;
-    DSU (int n) : n(n), par(1), sz(n + 1, 1) { // 1-based
-        for (int i = 1; i <= n; i++) par.pb(i);
+    DSU (int n) : n(n), par(n + 1), sz(n + 1, 0) {    // 1-based indexation
+        iota(par.begin(), par.end(), 0);
     }
     int find (int u) {  // representative upon u's connected component
-        if (par[u] == u) {
-            return u;
-        } else {
-            return par[u] = find(par[u]);
-        }
+        return (par[u] == u ? u : par[u] = find(par[u]));
     }
     bool connected (int u, int v) {  // whether u and v are in the same connected component
         return (find(u) == find(v));
     }
-    int count (int u) { return sz[find(u)]; }   // number of members among u's connected component
-    void merge (int u, int v) {    // unify (permanently) two connected components
-        u = find(u);
-        v = find(v);
+    void merge (int u, int v) {     // unify (permanently) two connected components
+        u = find(u); v = find(v);
         if (u != v) {
             n--;
             if (sz[u] < sz[v]) swap(u, v);
@@ -32,4 +26,5 @@ struct DSU {         // union by size
             sz[u] += sz[v];
         }
     }
+    int count (int u) { return sz[find(u)]; }  // number of members among u's connected component
 };
