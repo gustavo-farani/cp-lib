232577233f2fe757d31e403d7d209c33dcc4e45b
Gustavo F. de Farias <gff@cin.ufpe.br>
2020-03-24 16:13:18 -0300

    Inputing doubles as strings and scaling them to integers

---
+++ computational-geometry/2D/integer/scale-floating-point-to-integer.cpp
@@ -0,0 +1,32 @@
+#include <string>
+using namespace std;
+
+typedef long long ll;
+
+/* r: (representation) string representation of floating-point
+number to be converted. It can either have a decimal point or not. */
+/* p: (precision) maximum number of decimal positions after the decimal
+point in r */
+ll scale (const string& r, int p) {
+    ll ans = 0LL;
+    int i = 0;
+    while (i < r.size() && r[i] != '.') {
+        ans *= 10LL;
+        ans += r[i++] - '0';
+    }
+    int j = 0;
+    while (++i < r.size()) {
+        ans *= 10LL;
+        ans += r[i] - '0';
+        j++;
+    }
+    while (j++ < p) ans *= 10LL;
+    return ans;
+}
+
+/* shorter alternative: (it is not sure that will work)
+#include <cmath>
+
+ll scale (double x, int p) {
+    return llrint(pow(10.0, p)*x);
+} */
