dab13b4710fe117a2694b3f65122c485ff5349ef
Gustavo F. de Farias <gff@cin.ufpe.br>
2020-03-26 13:43:55 -0300

    Fixed bugs on DSU

--- data-structures/disjoint-sets-union/component-size.cpp
+++ data-structures/disjoint-sets-union/component-size.cpp
@@ -7,14 +7,14 @@ typedef vector<int> vi;
 
 struct DSU {       // union by size
     vi par, sz;
-    DSU (int n) : par(n + 1), sz(n + 1, 0) {    // 1-based indexation
+    DSU (int n) : par(n + 1), sz(n + 1, 1) {    // 1-based indexation
         iota(par.begin(), par.end(), 0);
     }
     int find (int u) {  // representative upon u's connected component
-        return (par[u] == u ? u : par[u] = find(par[u]));
+        return par[u] == u ? u : par[u] = find(par[u]);
     }
     bool connected (int u, int v) {  // whether u and v are in the same connected component
-        return (find(u) == find(v));
+        return find(u) == find(v))
     }
     void merge (int u, int v) {     // unify (permanently) two connected components
         u = find(u); v = find(v);
