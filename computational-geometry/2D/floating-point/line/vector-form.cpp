1141e745e59623dd4cef2932b32c74929904b16c
Gustavo F. de Farias <gff@cin.ufpe.br>
2020-04-16 14:31:29 -0300

    separated member functions of Line objects related to line-intersection in a single file

--- computational-geometry/2D/floating-point/line/vector-form.cpp
+++ computational-geometry/2D/floating-point/line/vector-form.cpp
@@ -8,16 +8,4 @@ struct Line {
     bool contains (const PT& q) const {
         return eq0(q - p ^ d);
     }
-    bool operator== (const Line& o) const {
-        return eq0(d ^ o.d) && o.contains(p);
-    }  // whether the lines coincide (have all of their points in commom)
-    bool concurrent (const Line &o) const {
-        return !eq0(d ^ o.d);
-    }  // whether the lines intersect in a single point
-    PT operator& (const Line& o) const {
-        if (concurrent(o)) {
-            ld t = (o.p - p ^ o.d)/(d ^ o.d);
-            return p + d*t;
-        } else throw -1;
-    }  // compute single point intersection of concurrent lines
 };
