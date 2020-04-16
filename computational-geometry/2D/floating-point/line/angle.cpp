7354319c5b59926a264ef67bd8725f2382cd7977
Gustavo F. de Farias <gff@cin.ufpe.br>
2020-04-16 14:29:49 -0300

    angle is no longer static, now is member function of Line objects

--- computational-geometry/2D/floating-point/line/angle.cpp
+++ computational-geometry/2D/floating-point/line/angle.cpp
@@ -1,13 +1,11 @@
 #include <cmath>
-#include "../vector-form.cpp"
-
-typedef double ld;
+#include "vector-form.cpp"
 
 const ld PI = acos(-1.0);
 
 // angle of a line measured counter-clock-wise from the x-axis
 // return value ranges in [0, PI)
-ld angle (const Line& l) {
-    ld a = atan2(l.d.x, l.d.y);
-    return (a < 0 ? a + PI : a);
+ld Line::angle () const {
+    ld a = atan2(d.x, d.y);
+    return a < 0 ? a + PI : a;
 }
