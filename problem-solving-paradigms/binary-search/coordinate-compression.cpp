4156d9ec50dd9bccc20ce84ed27c4509c50f2f5b
Gustavo F. de Farias <gff@cin.ufpe.br>
2020-04-19 04:49:35 -0300

    move constructor for the CoordinateCompression struct

--- problem-solving-paradigms/binary-search/coordinate-compression.cpp
+++ problem-solving-paradigms/binary-search/coordinate-compression.cpp
@@ -7,7 +7,7 @@ using namespace std;
 template<typename T>
 struct CoordinateCompression {
     vector<T> o;
-    void operator+= (const T &x) { o.pb(x); }
+    void push (T&& x) { o.pb(x); }  // moved
     int build () {
         sort(o.begin(), o.end());
         o.resize(unique(o.begin(), o.end()) - o.begin());
@@ -15,7 +15,7 @@ struct CoordinateCompression {
     }
     int operator() (const T &x) {   // 0-based
         auto it = lower_bound(o.begin(), o.end(), x);
-        return (it == o.end() || x < *it ? -1 : it - o.begin());
+        return it == o.end() || x < *it ? -1 : it - o.begin();
     }
     T& operator[] (int i) { return o[i]; }  // 0-based
 };
