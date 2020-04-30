e6f039e9c59a2ffeaf5312fa2903d16c341f1e37
Gustavo F. de Farias <gff@cin.ufpe.br>
2020-04-30 03:47:01 -0300

    Specific code and better written code for range max/min query segment trees

--- data-structures/segment-tree/basic/range-maximum-query.cpp
+++ data-structures/segment-tree/basic/range-maximum-query.cpp
@@ -1,9 +1,31 @@
 #include "../../../template.cpp"
 
-struct Node {
-    ll val;
-    Node operator+ (Node o) {
-        return Node{max(val, o.val)};
+template<class T>
+struct RangeMax {
+    vector<T> tree;
+    RangeMax (int n) : tree(n << 2, numeric_limits<T>::min()) {}
+    void update (int i, int l, int r, int p, T x) {
+        if (p > r || p < l) {
+            return;
+        } else if (l == r) {
+            tree[i] = x;
+        } else {
+            int m = l + r >> 1;
+            int lc = i << 1, rc = (i << 1) + 1;
+            update(lc, l, m, p, x);
+            update(rc, m + 1, r, p, x);
+            tree[i] = max(tree[lc], tree[rc]);
+        }
     }
-    void leaf (ll x) { val = x; }
-} NIL{LLONG_MIN};
+    T query (int i, int l, int r, int ql, int qr) {
+        if (r < ql || l > qr) {
+            return numeric_limits<T>::min();
+        } else if (ql <= l && r <= qr) {
+            return tree[i];
+        } else {
+            int m = l + r >> 1;
+            int lc = i << 1, rc = (i << 1) + 1;
+            return max(query(lc, l, m, ql, qr), query(rc, m + 1, r, ql, qr));
+        }
+    }
+};
--- data-structures/segment-tree/basic/range-minimum-query.cpp
+++ data-structures/segment-tree/basic/range-minimum-query.cpp
@@ -1,9 +1,31 @@
 #include "../../../template.cpp"
 
-struct Node {
-    ll val;
-    Node operator+ (Node o) {
-        return Node{min(val, o.val)};
+template<class T>
+struct RangeMin {
+    vector<T> tree;
+    RangeMin (int n) : tree(n << 2, numeric_limits<T>::max()) {}
+    void update (int i, int l, int r, int p, T x) {
+        if (p > r || p < l) {
+            return;
+        } else if (l == r) {
+            tree[i] = x;
+        } else {
+            int m = l + r >> 1;
+            int lc = i << 1, rc = (i << 1) + 1;
+            update(lc, l, m, p, x);
+            update(rc, m + 1, r, p, x);
+            tree[i] = min(tree[lc], tree[rc]);
+        }
     }
-    void leaf (ll x) { val = x; }
-} NIL{LLONG_MAX};
+    T query (int i, int l, int r, int ql, int qr) {
+        if (r < ql || l > qr) {
+            return numeric_limits<T>::max();
+        } else if (ql <= l && r <= qr) {
+            return tree[i];
+        } else {
+            int m = l + r >> 1;
+            int lc = i << 1, rc = (i << 1) + 1;
+            return min(query(lc, l, m, ql, qr), query(rc, m + 1, r, ql, qr));
+        }
+    }
+};
