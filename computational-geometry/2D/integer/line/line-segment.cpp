ca4eed1b65fc09f2d2635e1a46fb97ef8b1f0876
Gustavo F. de Farias <gff@cin.ufpe.br>
2020-05-07 15:57:49 -0300

    Added CLRS algorithm for computing line-segment intersection (integer coordinates)

--- computational-geometry/2D/integer/line/line-segment.cpp
+++ computational-geometry/2D/integer/line/line-segment.cpp
@@ -1,11 +1,23 @@
 #include "../vector/point.cpp"
 #include "../vector/orientation.cpp"
 
-struct LineSegment {  // TO BE TESTED
-    pair<PT, PT> p;
-    LineSegment (const PT& a, const PT& b) : p(a, b) {}
-    bool contains (const PT& o) {
-        return colinear(p.first, p.second, o)
-        && (o - p.first)*(o - p.second) <= 0;
-    }
+struct LineSegment {
+    PT a, b;
+    LineSegment (const PT& a, const PT& b) : a(a), b(b) {}
+    bool contains (const PT& o) const {
+        ll l, r, u, d;
+        tie(l, r) = minmax(a.x, b.x);
+        tie(d, u) = minmax(a.y, b.y);
+        return l <= o.x && o.x <= r && d <= o.y && o.y <= u;
+    }  // point o is assumed to be colinear with a and b
+    bool operator&& (const LineSegment& o) {
+        PT u = b - a, v = o.b - o.a;
+        ll ua = u ^ o.a-a, ub = u ^ o.b-a;
+        ll va = v ^ a-o.a, vb = v ^ b-o.b;
+        return ua*ub < 0 && va*vb < 0
+        || ua == 0 && contains(o.a)
+        || ub == 0 && contains(o.b)
+        || va == 0 && o.contains(a)
+        || vb == 0 && o.contains(b);
+    }  // test line-segment intersection
 };
