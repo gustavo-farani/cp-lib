5dcde3c331bd615a4c080dc5dd6de2d874f8b260
Gustavo F. de Farias <gff@cin.ufpe.br>
2020-05-17 01:13:32 -0300

    Added Mobius function

---
+++ math/number-theory/sieve-of-eratosthenes/mobius-function.cpp
@@ -0,0 +1,19 @@
+#include "../../../template.cpp"
+
+struct Mobius {
+    vi mu;
+    Mobius (int n) : mu(n + 1, 1) {
+        vector<bool> sieve(n + 1, true);
+        for (int i = 2; i <= n; i++) {
+            if (sieve[i]) {
+                for (int j = i; j <= n; j += i) {
+                    mu[j] *= -1;
+                    sieve[j] = false;
+                }
+                ll sq = (ll) i*i;
+                for (ll j = sq; j <= n; j += sq) mu[j] = 0;
+            }
+        }
+    }
+    int operator() (int n) { return mu[n]; }
+} mu(1e5);
