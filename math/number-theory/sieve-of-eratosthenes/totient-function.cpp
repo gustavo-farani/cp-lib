d0058d2b4c2d3026b89d3dbee09783f785f5f322
Gustavo F. de Farias <gff@cin.ufpe.br>
2020-05-17 01:12:51 -0300

    Simplified code for totient function

--- math/number-theory/sieve-of-eratosthenes/totient-function.cpp
+++ math/number-theory/sieve-of-eratosthenes/totient-function.cpp
@@ -1,17 +1,16 @@
 #include "../../../template.cpp"
 
 struct Totient {
-    vector<ll> num, den;
-    Totient (int lim) : num(lim + 1), den(lim + 1) {
-        for (int i = 1; i <= lim; i++) num[i] = den[i] = 1;
-        for (int i = 2; i <= lim; i++) {
-            if (den[i] == 1LL) {
-                for (int j = i; j <= lim; j += i) {
+    vi num, den;
+    Totient (int n) : num(n + 1, 1), den(n + 1, 1) {
+        for (int i = 2; i <= n; i++) {
+            if (den[i] == 1) {
+                for (int j = i; j <= n; j += i) {
                     num[j] *= i - 1;
                     den[j] *= i;
                 }
             }
         }
     }
-    ll operator() (int n) { return n*num[n]/den[n]; }
+    int operator() (int n) { return n*num[n]/den[n]; }
 };
