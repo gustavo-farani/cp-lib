7fe0b7ff4a344352a40e3bb0176d0db3d5d6d333
Gustavo F. de Farias <gff@cin.ufpe.br>
2020-06-20 18:48:10 -0300

    Dijkstra with __gnu_pbds heap

--- graph/sparse/neighbour-list/shortest-paths/single-source/dijkstra.cpp
+++ graph/sparse/neighbour-list/shortest-paths/single-source/dijkstra.cpp
@@ -1,35 +1,47 @@
 #include "../../representation/weighted-graph.cpp"
+#include <ext/pb_ds/priority_queue.hpp>
+template<class T> using min_heap = __gnu_pbds::priority_queue<T, greater<T>>;
 
-vector<ll> minimumDistances (const WeightedGraph& g, int s) {
-    vector<ll> dist(g.last, LLONG_MAX);
-    min_heap<pair<ll, int>> pq;
-    pq.emplace(dist[s] = 0, s);
+template<class T>
+vector<T> minimumDistances (const WeightedGraph& g, int s) {
+    vector<T> dist(g.last, numeric_limits<T>::max());
+    min_heap<pair<T, int>> pq;
+    vector<typename min_heap<pair<T, int>>::point_iterator> at(g.first);
+    for (int v = g.first; v < g.last; v++) {
+        pair<T, int> e;
+        dist[v] = e.first = v == s ? 0 : numeric_limits<T>::max();
+        e.second = v;
+        at.pb(pq.push(e));
+    }
     while (!pq.empty()) {
-        auto [l, u] = pq.top(); pq.pop();  // C++ 17
-        if (l <= dist[u]) {
-            for (auto [w, v] : g.adj[u]) {  // C++ 17
-                if (l + w < dist[v]) {
-                    pq.emplace(dist[v] = l + w, v);
-                }
-            }
+        auto [l, u] = pq.top(); pq.pop();  // C++ 17 only
+        if (l == numeric_limits<T>::max()) break;
+        for (auto [w, v] : g.adj[u]) {
+            if (l + w < dist[v]) pq.modify(at[v], {dist[v] = l + w, v});
         }
     }
     return dist;
 }
 
+template<class T>
 vi shortestPaths (const WeightedGraph& g, int s) {
-    vector<ll> dist(g.last, LLONG_MAX);
+    vector<T> dist(g.last, numeric_limits<T>::max());
     vi from(g.last, -1);
-    min_heap<pair<ll, int>> pq;
-    pq.emplace(dist[s] = 0, s);
+    min_heap<pair<T, int>> pq;
+    vector<typename min_heap<pair<T, int>>::point_iterator> at(g.first);
+    for (int v = g.first; v < g.last; v++) {
+        pair<T, int> e;
+        dist[v] = e.first = v == s ? 0 : numeric_limits<T>::max();
+        e.second = v;
+        at.pb(pq.push(e));
+    }
     while (!pq.empty()) {
-        auto [l, u] = pq.top(); pq.pop();  // C++ 17
-        if (l <= dist[u]) {
-            for (auto [w, v] : g.adj[u]) {  // C++ 17
-                if (l + w < dist[v]) {
-                    from[v] = u;
-                    pq.emplace(dist[v] = l + w, v);
-                }
+        auto [l, u] = pq.top(); pq.pop();  // C++ 17 only
+        if (l == numeric_limits<T>::max()) break;
+        for (auto [w, v] : g.adj[u]) {
+            if (l + w < dist[v]) {
+                pq.modify(at[v], {dist[v] = l + w, v});
+                from[v] = u;
             }
         }
     }
