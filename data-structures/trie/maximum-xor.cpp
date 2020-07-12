ce2adad17f1962e28a57f87bdd1c6cfbbe990b74
Gustavo F. de Farias <gff@cin.ufpe.br>
2020-07-12 11:44:05 -0300

    XOR-Trie

---
+++ data-structures/trie/maximum-xor.cpp
@@ -0,0 +1,42 @@
+#include "../../template.cpp"
+
+const int N = 1e5 + 5;
+const int MS = N << 5, MSB = 1 << 30;
+int z, sz, adj[MS][2], term[MS];
+
+// x: non-negative integer
+// inserts number x into the trie
+void insert (int x) {
+    int cs = 0;
+    for (int b = MSB; b; b >>= 1) {
+        bool i = x & b;
+        if (adj[cs][i] == 0) adj[cs][i] = z++;
+        cs = adj[cs][i];
+    }
+    // the number inserted later overrides its predecessor (if any)
+    // the timer is 1-based
+    term[cs] = ++sz;
+}
+
+// x: non-negative integer
+// ans: number in trie such that x ^ ans is maximum
+// t: is assigned the latest time when number ans was inserted
+int maxXor (int x, int &t) {
+    int ans = 0, cs = 0;
+    for (int b = MSB; b; b >>= 1) {
+        bool i = x & b;
+        i ^= adj[cs][!i] > 0;
+        if (i) ans |= b;
+        cs = adj[cs][i];
+    }
+    t = term[cs];
+    return ans;
+}
+
+void init () {
+    z = 0;
+    sz = -1;
+    memset(term, 0, sizeof term);
+    memset(adj, 0, sizeof adj);
+    insert(0);
+}
