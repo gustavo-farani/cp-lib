7bbde316913d05922bb1d26556d3c919a771d376
Gustavo F. de Farias <gff@cin.ufpe.br>
2020-07-22 17:27:48 -0300

    Angle between two vectors

--- computational-geometry/2D/floating-point/vector/angle.cpp
+++ computational-geometry/2D/floating-point/vector/angle.cpp
@@ -1,7 +1,8 @@
 #include "point.cpp"
 
 const double PI = acos(-1.0);
-double arg (PT p) { return atan2(p.y, p.x); }
-PT rot (PT v, double a) {  // CCW
+double angle (PT p) { return atan2(p.y, p.x); }
+double angle (PT u, PT v) { return atan2(cross(u, v), dot(u, v)); }
+PT rotate (PT v, double a) {  // CCW
     return PT(v.x*cos(a) - v.y*sin(a), v.x*sin(a) + v.y*cos(a));
 }
