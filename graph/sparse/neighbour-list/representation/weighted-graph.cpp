4b9e9eba2ea2ba54d02e11d433123bd3e36ae07a
Gustavo F. de Farias <gff@cin.ufpe.br>
2020-09-19 11:50:37 -0300

    emplace_back instead of push_back

--- graph/sparse/neighbour-list/representation/weighted-graph.cpp
+++ graph/sparse/neighbour-list/representation/weighted-graph.cpp
@@ -2,17 +2,17 @@
 
 struct Graph {
     int first, last, n;
-    vector<vector<pair<ll, int>>> adj;
+    vector<vector<ii>> adj;
     // base == 0: vertices indices in range [0, n)
     // base == 1: vertices indices in range [1, n]
     Graph (int n, bool b) :
         first(b), last(n + b), n(n), adj(last)
     {}
-    void addEdge (int u, int v, ll w) {
-        adj[u].pb({w, v});
-        adj[v].pb({w, u});  // for undirected graph
+    void addEdge (int u, int v, int w) {
+        adj[u].emplace_back(w, v);
+        adj[v].emplace_back(w, u);  // for undirected graph
     }
-    void addArc (int u, int v, ll w) {
-        adj[u].pb({w, v});  // for directed graph
+    void addArc (int u, int v, int w) {
+        adj[u].emplace_back(w, v);  // for directed graph
     }
 };
