56940bd9abcfd74156486f0150ed450e03f4f29d
Gustavo F. de Farias <gff@cin.ufpe.br>
2020-09-19 11:42:48 -0300

    Iterative segment tree as object

--- data-structures/segment-tree/basic/iterative.cpp
+++ data-structures/segment-tree/basic/iterative.cpp
@@ -1,43 +1,44 @@
-namespace SegmentTree {
-    // smallest power of 2 greater than maximum array size
-    const int N = 131072;
-    // base-indexation for the indices (either 0 or 1)
-    const bool B = 0;
+#include "../../../boilerplate.cpp"
+#include "../../../math/bitmasks/lg.cpp"
+
+template<bool B, class T>
+struct SegmentTree {
     struct Node {
         // binary operation for getting the parent node
         // by merging its left and right children
         // operation must be associative and run in constant time
-        Node(const Node& l, const Node& r);
+        Node (const Node& l, const Node& r) {}
         // the default initialization should correspond
         // to the neutral operand in the binary operation
-        Node();
+        Node () {}
         // for updating a leaf node
-        Node(int x);
-    } tree[N << 1];
+        Node (const T& x) {}
+    };
+    int n;
+    vector<Node> tree;
+    SegmentTree (int n) : n(1 << ceilLg(n)), tree(this->n << 1) {}
     // updates a leaf
-    void at (int i, int x) { tree[i - B + N] = x; }
+    void at (int i, const T& x) { tree[i - B + n] = x; }
     // initialization of the whole tree
     void build () {
-        for (int p = N; p--;) tree[p] = {tree[p << 1], tree[p << 1 | 1]};
+        for (int p = n; p--;) tree[p] = {tree[p << 1], tree[p << 1 | 1]};
     }
     // updates a leaf and every ancestor of it
-    void update (int i, int x) {
+    void update (int i, const T& x) {
         at(i, x);
         i -= B;
-        for (int p = i + N >> 1; p > 0; p >>= 1) {
+        for (int p = i + n >> 1; p > 0; p >>= 1) {
             tree[p] = {tree[p << 1], tree[p << 1 | 1]};
         }
     }
-    // [ql, qr]: closed interval
+    // [l, r]: closed interval
     Node query (int l, int r) {
         l -= B, r -= B;
         Node lh, rh;
-        for (l += N, r += N + 1; l < r; l >>= 1, r >>= 1) {
+        for (l += n, r += n + 1; l < r; l >>= 1, r >>= 1) {
             if (l & 1) lh = {lh, tree[l++]};
             if (r & 1) rh = {tree[--r], rh};
         }
         return {lh, rh};
     }
-}
-
-using namespace SegmentTree;
+};
