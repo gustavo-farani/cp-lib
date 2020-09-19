373e08fb7d24a398c759380f15e3e364c5fb55ec
Gustavo F. de Farias <gff@cin.ufpe.br>
2020-09-19 11:49:59 -0300

    Changed variable name

--- data-structures/trie/maximum-xor.cpp
+++ data-structures/trie/maximum-xor.cpp
@@ -2,7 +2,7 @@
 
 const int N = 1e5 + 5;
 const int MS = N << 5, MSB = 1 << 30;
-int z, sz, adj[MS][2], term[MS];
+int z, sz, dfa[MS][2], term[MS];
 
 // x: non-negative integer
 // inserts number x into the trie
@@ -10,8 +10,8 @@ void insert (int x) {
     int cs = 0;
     for (int b = MSB; b; b >>= 1) {
         bool i = x & b;
-        if (adj[cs][i] == 0) adj[cs][i] = z++;
-        cs = adj[cs][i];
+        if (dfa[cs][i] == 0) dfa[cs][i] = z++;
+        cs = dfa[cs][i];
     }
     // the number inserted later overrides its predecessor (if any)
     // the timer is 1-based
@@ -25,9 +25,9 @@ int maxXor (int x, int &t) {
     int ans = 0, cs = 0;
     for (int b = MSB; b; b >>= 1) {
         bool i = x & b;
-        i ^= adj[cs][!i] > 0;
+        i ^= dfa[cs][!i] > 0;
         if (i) ans |= b;
-        cs = adj[cs][i];
+        cs = dfa[cs][i];
     }
     t = term[cs];
     return ans;
@@ -37,6 +37,6 @@ void init () {
     z = 0;
     sz = -1;
     memset(term, 0, sizeof term);
-    memset(adj, 0, sizeof adj);
+    memset(dfa, 0, sizeof dfa);
     insert(0);
 }
