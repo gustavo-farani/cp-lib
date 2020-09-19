c741a1ec481a1637674fccd4c2b35bac2df8a91f
Gustavo F. de Farias <gff@cin.ufpe.br>
2020-09-19 11:28:46 -0300

    Implicit constructor call on return

--- computational-geometry/2D/floating-point/circle/circumcircle.cpp
+++ computational-geometry/2D/floating-point/circle/circumcircle.cpp
@@ -4,6 +4,6 @@
 // computes the circumference spanning the triangle a-b-c
 Circle circumcircle (PT a, PT b, PT c) {
     PT u = b - a, v = c - a;
-    PT w = perp(u*dot(v, v) - v*dot(u, u))/cross(u, v)/2.0;
-    return Circle(a + w, abs(w));
+    PT w = perp(u*dot(v, v) - v*dot(u, u))/cross(u, v)/2.0L;
+    return {a + w, abs(w)};
 }
