b5d3bbd5a87aee7eaba9133ac8460a864ef589f9
Gustavo F. de Farias <gff@cin.ufpe.br>
2020-09-19 11:52:06 -0300

    Added multi-source dijkstra

--- graph/sparse/neighbour-list/shortest-paths/single-source/dijkstra.cpp
+++ graph/sparse/neighbour-list/shortest-paths/single-source/dijkstra.cpp
@@ -6,14 +6,28 @@ template<class T> using min_heap = __gnu_pbds::priority_queue<T, greater<T>>;
 template<class T>
 vector<T> minimumDistances (const Graph& g, int s) {
     vector<T> dist(g.last, numeric_limits<T>::max());
+    dist[s] = 0;
     min_heap<pair<T, int>> pq;
     vector<typename min_heap<pair<T, int>>::point_iterator> at(g.first);
-    for (int v = g.first; v < g.last; v++) {
-        pair<T, int> e;
-        dist[v] = e.first = v == s ? 0 : numeric_limits<T>::max();
-        e.second = v;
-        at.pb(pq.push(e));
+    for (int v = g.first; v < g.last; v++) at.pb(pq.push({dist[v], v}));
+    while (!pq.empty()) {
+        auto [l, u] = pq.top(); pq.pop();  // C++ 17 only
+        if (l == numeric_limits<T>::max()) break;
+        for (auto [w, v] : g.adj[u]) {
+            if (l + w < dist[v]) pq.modify(at[v], {dist[v] = l + w, v});
+        }
     }
+    return dist;
+}
+
+// multi-source
+template<class T>
+vector<T> minimumDistances (const Graph& g, vi& s) {
+    vector<T> dist(g.last, numeric_limits<T>::max());
+    for (int v : s) dist[v] = 0;
+    min_heap<pair<T, int>> pq;
+    vector<typename min_heap<pair<T, int>>::point_iterator> at(g.first);
+    for (int v = g.first; v < g.last; v++) at.pb(pq.push({dist[v], v}));
     while (!pq.empty()) {
         auto [l, u] = pq.top(); pq.pop();  // C++ 17 only
         if (l == numeric_limits<T>::max()) break;
@@ -27,15 +41,11 @@ vector<T> minimumDistances (const Graph& g, int s) {
 template<class T>
 vi shortestPaths (const Graph& g, int s) {
     vector<T> dist(g.last, numeric_limits<T>::max());
+    dist[s] = 0;
     vi from(g.last, -1);
     min_heap<pair<T, int>> pq;
     vector<typename min_heap<pair<T, int>>::point_iterator> at(g.first);
-    for (int v = g.first; v < g.last; v++) {
-        pair<T, int> e;
-        dist[v] = e.first = v == s ? 0 : numeric_limits<T>::max();
-        e.second = v;
-        at.pb(pq.push(e));
-    }
+    for (int v = g.first; v < g.last; v++) at.pb(pq.push({dist[v], v}));
     while (!pq.empty()) {
         auto [l, u] = pq.top(); pq.pop();  // C++ 17 only
         if (l == numeric_limits<T>::max()) break;
